5f28ee255397676d61aacc9b5f54232291bc2e16
Aayush Dhungel <adhungel0413@gmail.com>
2024-12-12 03:13:24 +0545

    authentication middleware

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express"
-import { register, login } from "../controllers/user.controllers.js"
+import { register, login, logout } from "../controllers/user.controllers.js"
+import isAuthenticated from "../middlewares/auth.middleware.js"
 
 const router = express.Router()
 
 router.post("/register", register)
 router.post("/login", login)
+router.get("/logout", logout)
+
+router.get("/dashboard", isAuthenticated)
 
 export default router
